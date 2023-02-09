a700e5f64d5a9363ddd74a4bc6e46a7bd3c5f9d0
dongseup_lim <dongseup83@gmail.com>
2023-02-09 17:48:21 +0900

    useRenderCount

---
+++ src/hooks/useRenderCount/useRenderCount.js
@@ -0,0 +1,9 @@
+import { useEffect, useRef } from 'react';
+
+export default function useRenderCount() {
+    const count = useRef(0);
+
+    useEffect(() => count.current++);
+
+    return count.current;
+}
